338fb542519439d889160dc21d2d738608f6e154
Yashodha2006 <yashodhabasarikatti21@gmail.com>
2026-01-07 13:24:42 +0530

    Update test_product.py

--- test_product.py
+++ test_product.py
@@ -1,9 +1,19 @@
-def test_product_details():
-    expected_output = (
-        "Product ID: P101\n"
-        "Product Name: Laptop\n"
-        "Quantity: 5\n"
-        "Price: 55000"
-    )
+import unittest
+from product import product_details
 
-    assert product_details("P101", "Laptop", 5, 55000) == expected_output
+class TestProductDetails(unittest.TestCase):
+
+    def test_product_details(self):
+        expected = (
+            "Product ID: P101\n"
+            "Product Name: Laptop\n"
+            "Quantity: 5\n"
+            "Price: 55000"
+        )
+
+        result = product_details("P101", "Laptop", 5, 55000)
+
+        self.assertEqual(result, expected)
+
+if __name__ == "__main__":
+    unittest.main()
